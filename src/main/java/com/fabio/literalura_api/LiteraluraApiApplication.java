0fe73ba12aac9e3d56332113c5ac6d6b7b913f87
Fabio Massaretto <fabio_no_more@yahoo.com.br>
2025-06-10 20:22:00 -0300

    initial commit

---
+++ src/main/java/com/fabio/literalura_api/LiteraluraApiApplication.java
@@ -0,0 +1,13 @@
+package com.fabio.literalura_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiteraluraApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiteraluraApiApplication.class, args);
+	}
+
+}
